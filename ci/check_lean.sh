f4e87e48f5850303ba9b2feb99bb2d4a361c8a58
Fyre Mael <fyremaelstrom@gmail.com>
2026-08-02 02:24:33 -0700

    Register Ehrhart adjudication controls

--- ci/check_lean.sh
+++ ci/check_lean.sh
@@ -39,5 +39,7 @@ python3 ci/validate_human_steward_post_merge_attestation.py
 python3 ci/test_human_steward_post_merge_attestation.py
 python3 ci/validate_openai_ten_proofs_adjudication_contracts.py
 python3 ci/test_openai_ten_proofs_adjudication_contracts.py
+python3 ci/validate_otp_ehrhart_adjudication.py
+python3 ci/test_otp_ehrhart_adjudication.py
 python3 ci/audit_ci_reachability.py
 python3 ci/test_audit_ci_reachability.py
